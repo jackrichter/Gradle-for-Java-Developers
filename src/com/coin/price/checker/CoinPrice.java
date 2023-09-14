78e4e0286bd631cc9356c5d141fbfc0020272597
Jack Richter <jack.richter@softronic.se>
2023-09-14 12:32:41 +0200

    Adding the 'java' plugin.

--- src/com/coin/price/checker/CoinPrice.java
+++ src/com/coin/price/checker/CoinPrice.java
@@ -2,6 +2,6 @@ package com.coin.price.checker;
 
 public class CoinPrice {
     public static void main(String[] args) {
-
+        System.out.println("Welcome to digital coin price checker");
     }
 }
