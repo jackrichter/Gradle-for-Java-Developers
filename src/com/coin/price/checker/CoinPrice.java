4305afef4c9a43cc840d4fc3dc0040d4075d459c
jackrichter <jackrichter1@gmail.com>
2023-09-26 18:20:01 +0200

    Build Java project with Gradle.

--- src/com/coin/price/checker/CoinPrice.java
+++ src/com/coin/price/checker/CoinPrice.java
@@ -1,7 +1,19 @@
 package com.coin.price.checker;
 
+import org.jsoup.Jsoup;
+
+import java.io.IOException;
+
 public class CoinPrice {
     public static void main(String[] args) {
         System.out.println("Welcome to digital coin price checker");
+
+        final String BTC = "https://api.coinbase.com/v2/prices/BTC-USD/spot";
+        try {
+            String val = Jsoup.connect(BTC).ignoreContentType(true).execute().body();
+            System.out.println(val);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
